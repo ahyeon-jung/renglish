5208754aa17c5fb49b6a7578d1605bceb008a749
a-honey <jungahyeon0512@khu.ac.kr>
2025-03-11 19:14:19 +0900

    fix: LayoutProps 타입에 components 추가
    build error

--- client/src/app/@components/LatestScripts/index.tsx
+++ client/src/app/@components/LatestScripts/index.tsx
@@ -2,7 +2,7 @@ import Container from "../Container";
 
 export default function LatestScript() {
   return (
-    <Container label="Script Categories">
+    <Container label="Latest Script">
       <div className="flex flex-col bg-gray-300 p-4 rounded-xl">
         <div>타이틀</div>
         <div>별점</div>
