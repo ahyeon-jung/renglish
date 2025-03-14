182afbd51a160149099981a408bd2c86ed52ef7a
a-honey <jungahyeon0512@khu.ac.kr>
2025-03-14 14:43:46 +0900

    feat: 언어 변경시 스크롤 유지
    sessionStorage에 저장 후 삭제하는 방식

--- client/src/app/movies/[movie]/scripts/[scene]/@components/Header/index.tsx
+++ client/src/app/movies/[movie]/scripts/[scene]/@components/Header/index.tsx
@@ -1,22 +1,35 @@
 "use client";
 
+import { restoreScrollPosition, saveScrollPosition } from "@/utils/scroll";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Indicator from "@/components/Indicator";
 import { LANGUAGE_MODE } from "@/constants/language";
 import SubHeaderContainer from "@/components/SubheaderContainer";
 import { formatTitle } from "@/utils/format";
+import { useEffect } from "react";
 
 type Header = { title: string };
 
 export default function Header({ title }: Header) {
   const router = useRouter();
+
   const searchParams = useSearchParams();
   const mode = searchParams.get("mode");
 
   const changeModeToLanguage = (value: string) => {
+    saveScrollPosition();
     router.replace(`?mode=${value}`);
   };
+
+  useEffect(() => {
+    const restoreScrollWithDelay = () => {
+      window.requestAnimationFrame(() => {
+        restoreScrollPosition();
+      });
+    };
+    restoreScrollWithDelay();
+  }, [mode]);
 
   return (
     <SubHeaderContainer title={formatTitle(title)}>
---
+++ client/src/utils/scroll.ts
@@ -0,0 +1,13 @@
+const SCROLL_POSITION_KEY = "scrollPosition";
+
+export function saveScrollPosition() {
+  sessionStorage.setItem(SCROLL_POSITION_KEY, window.scrollY.toString());
+}
+
+export function restoreScrollPosition() {
+  const scrollPos = sessionStorage.getItem(SCROLL_POSITION_KEY);
+  if (scrollPos) {
+    window.scrollTo(0, Number(scrollPos));
+    sessionStorage.removeItem(SCROLL_POSITION_KEY);
+  }
+}
