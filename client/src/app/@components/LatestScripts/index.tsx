c7b5395813a63fba0d500e55595e7babeafbda71
a-honey <jungahyeon0512@khu.ac.kr>
2025-03-19 03:39:54 +0900

    feat: 배너 정적 이미지 추가

--- client/src/app/@components/LatestScripts/index.tsx
+++ client/src/app/@components/LatestScripts/index.tsx
@@ -1,13 +1,27 @@
 import Container from "../Container";
+import Image from "next/image";
+import Overlay from "@/components/Overlay";
+import Text from "@/components/Text";
 
 export default function LatestScript() {
   return (
     <Container label="Latest Script">
-      <div className="flex flex-col bg-gray-300 p-4 rounded-xl">
-        <div>타이틀</div>
-        <div>별점</div>
+      <div className="relative h-[100px] overflow-hidden rounded-xl">
+        <Text
+          className="absolute right-4 bottom-0 text-white z-[var(--overlay-text-z-index)]"
+          typography="display-xl"
+        >
+          Inside Out
+        </Text>
+        <Overlay />
+        <Image
+          alt="inside out poster"
+          src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDA3MDlfMjkx%2FMDAxNzIwNTIzNzgwMDQ1.Z7Q_Mx9F_aj4Gy4JTNZJxrRrh9M9sGT3_WoZmlF3k3cg.e6x1PsoFbfhuVNzWyYzXFSm4pGkKN4CD3yYYl2Gwq48g.JPEG%2FIMG_9355.JPG&type=sc960_832"
+          width={500}
+          height={100}
+          className="absolute bottom-[-200px] object-cover object-bottom"
+        />
       </div>
-      <div>이미지</div>
     </Container>
   );
 }
