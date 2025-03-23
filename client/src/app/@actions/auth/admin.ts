3eb270cdeaba39ddb8666892b992536063649292
a-honey <jungahyeon0512@khu.ac.kr>
2025-03-24 03:23:07 +0900

    fix: 관리자 토큰 확인 서버액션 401 에러시 잡아서 false 반환

--- client/src/app/@actions/auth/admin.ts
+++ client/src/app/@actions/auth/admin.ts
@@ -4,20 +4,24 @@ import { cookies } from 'next/headers';
 import { fetchAPI } from '@/libs/api';
 
 export default async function adminAction() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get('token')?.value;
+  try {
+    const cookieStore = await cookies();
+    const token = cookieStore.get('token')?.value;
 
-  const response = await fetchAPI<{ isAdmin: boolean }>(`/auth/check/admin`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-  });
+    const response = await fetchAPI<{ isAdmin: boolean }>(`/auth/check/admin`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  const {
-    data: { isAdmin },
-  } = response;
+    const {
+      data: { isAdmin },
+    } = response;
 
-  return isAdmin;
+    return isAdmin;
+  } catch {
+    return false;
+  }
 }
