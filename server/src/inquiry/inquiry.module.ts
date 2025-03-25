455319a0a1a19d231b7904b33d6102db88ba58c1
a-honey <jungahyeon0512@khu.ac.kr>
2025-03-26 01:54:21 +0900

    fix: inquiry module imports 수정

--- server/src/inquiry/inquiry.module.ts
+++ server/src/inquiry/inquiry.module.ts
@@ -4,10 +4,12 @@ import { InquiryController } from './inquiry.controller';
 import { InquiryService } from './inquiry.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Inquiry])],
-  controllers: [InquiryController, AccessTokenStrategy],
-  providers: [InquiryService],
+  imports: [TypeOrmModule.forFeature([Inquiry, User])],
+  controllers: [InquiryController],
+  providers: [InquiryService, UserService, AccessTokenStrategy],
 })
 export class InquiryModule {}
