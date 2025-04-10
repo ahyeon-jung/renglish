c95c21e61a2ec43bcf6a05db7a9a714ee298c6c0
a-honey <jungahyeon0512@khu.ac.kr>
2025-04-11 06:07:28 +0900

    feat: swagger 기반 코드 자동 생성 추가 (#58)

---
+++ client/src/services/models/index.ts
@@ -0,0 +1,27 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ChangePasswordDto';
+export * from './CreateDialogueDto';
+export * from './CreateExpressionDto';
+export * from './CreateMovieDto';
+export * from './CreateSceneDto';
+export * from './CreateSpeakerDto';
+export * from './CreateStudyDto';
+export * from './CreateUserDto';
+export * from './CreateWritingDto';
+export * from './ExampleDto';
+export * from './LoginDto';
+export * from './Movie';
+export * from './PaginationMovieResponseDto';
+export * from './PaginationUserResponseDto';
+export * from './Scene';
+export * from './SendEmailDto';
+export * from './Study';
+export * from './UpdateDialogueDto';
+export * from './UpdateExpressionDto';
+export * from './UpdateMovieImageDto';
+export * from './UpdateSceneDto';
+export * from './UpdateSpeakerDto';
+export * from './UpdateStudyDto';
+export * from './UpdateUserDto';
+export * from './VerifyCodeDto';
