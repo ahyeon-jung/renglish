ad208d7d6070dc98e3ab25e44b131297ec089786
a-honey <jungahyeon0512@khu.ac.kr>
2025-04-11 19:46:25 +0900

    feat: 클라이언트 소셜 로그인 UI 추가

---
+++ client/src/app/auth/_components/SocialButtons/index.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import kakao_logo from '@/assets/social/kakao_logo.svg';
+import google_logo from '@/assets/social/google_logo.svg';
+import naver_logo from '@/assets/social/naver_logo.svg';
+import clsx from 'clsx';
+
+const SOCIAL_LOGINS = [
+  {label: 'Google', icon: google_logo, className: "bg-[#FFFFFF] border border-[#747775]"},
+  {label: 'Kakao', icon: kakao_logo, className: "bg-[#FEE500]"},
+  {label: 'Naver', icon: naver_logo, className: "gap-3 bg-[#03C75A] text-white"},
+]
+
+type SocialButtonsProps = {
+  type: 'login' | 'register';
+}
+
+export default function SocialButtons({type}: SocialButtonsProps) {
+  return (
+   <div className='flex flex-col gap-2'>
+    {SOCIAL_LOGINS.map(({label, icon, className}) => (
+      <div 
+        key={label}
+        className={clsx(
+          `p-3 rounded-md gap-2`,
+          'flex justify-center',
+          className
+      )}>
+      <Image src={icon} alt="kakao" width={20} height={20} />
+      <div>{type === 'login' ? 'Login with' : 'Register with'} {label}</div>
+     </div>
+    ))}
+   </div>
+  );
+}
