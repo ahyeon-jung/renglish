d86bfe0ee5d83957be309595a425455f3b883791
a-honey <jungahyeon0512@khu.ac.kr>
2025-04-17 22:12:46 +0900

    fix: JSX.Eelement 지정

--- apps/client/src/components/Text/index.tsx
+++ apps/client/src/components/Text/index.tsx
@@ -44,7 +44,7 @@ export default function Text({
   as = 'span',
   typography,
   ...props
-}: TextProps) {
+}: TextProps): React.JSX.Element {
   const classes = typography ? typographyClasses[typography] : '';
 
   return React.createElement(as, { ...props, className: clsx(classes, className) }, children);
