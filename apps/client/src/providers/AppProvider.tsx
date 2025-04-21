f2d7fb35e72a2dd22198da0fbfc697a52198c78b
a-honey <jungahyeon0512@khu.ac.kr>
2025-04-21 13:31:55 +0900

    feat: react-query devtool 설치

--- apps/client/src/providers/AppProvider.tsx
+++ apps/client/src/providers/AppProvider.tsx
@@ -3,10 +3,16 @@
 import { PropsWithChildren } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import getQueryClient from '@/libs/getQueryClient';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function AppProvider({ children }: PropsWithChildren) {
   const queryClient = getQueryClient();
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
+  );
 }
 
 export default AppProvider;
