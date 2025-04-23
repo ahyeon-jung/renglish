25f3f1f9fbb482a1464f89c28ce7b1aeabc073f2
a-honey <jungahyeon0512@khu.ac.kr>
2025-04-24 08:55:11 +0900

    feat: 장면페이지 정보 및 목록 이동 아이콘 추가 (#102)

--- apps/client/src/app/movies/[movie]/[scene]/_components/SceneHeader/SceneHeader.tsx
+++ apps/client/src/app/movies/[movie]/[scene]/_components/SceneHeader/SceneHeader.tsx
@@ -3,14 +3,27 @@ import SceneNav from '../SceneNav';
 import SubHeaderContainer from '@/components/SubheaderContainer';
 import { formatTitle } from '@/utils/format';
 import { SceneNavProps } from '../SceneNav/SceneNav';
+import { AlignJustify, Info as InfoIcon } from 'lucide-react';
+import Link from 'next/link';
+import { PATHS } from '@/constants/path';
 
-type Header = { title: string } & SceneNavProps & React.PropsWithChildren;
+type Header = { title: string } & Omit<SceneNavProps, 'iconSize'> & React.PropsWithChildren;
 
-export default function SceneHeader({ title, children, ...props }: Header) {
+export default function SceneHeader({ title, children, movieId, sceneId }: Header) {
+  const iconSize = 21;
+
   return (
     <SubHeaderContainer title={formatTitle(title)}>
       {children}
-      <SceneNav {...props} />
+      <div className="flex gap-2 items-center">
+        <Link href={PATHS.MOVIE.DETAIL(movieId)} title="영화 상세 정보">
+          <InfoIcon size={iconSize} />
+        </Link>
+        <Link href={PATHS.MOVIE.LIST} title="목록으로 돌아가기">
+          <AlignJustify size={iconSize} />
+        </Link>
+        <SceneNav iconSize={iconSize} movieId={movieId} sceneId={sceneId} />
+      </div>
     </SubHeaderContainer>
   );
 }
