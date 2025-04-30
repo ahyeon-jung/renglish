3b6524c2ef0c5c242973237fce1cbb030120cb12
a-honey <jungahyeon0512@khu.ac.kr>
2025-05-01 00:31:27 +0900

    feat: 페이지네이션 조회 infiniteQuery로 교체

--- apps/client/src/app/movies/Movies.tsx
+++ apps/client/src/app/movies/Movies.tsx
@@ -1,52 +1,46 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
 import Button from '@/components/Button';
 import Categories from './_components/Categories';
-import { ENV } from '@/constants/env';
-import { Movie } from '@/types/movie';
 import ScriptListItem from './_components/ScriptListItem';
 import clsx from 'clsx';
-import useSWRInfinite from 'swr/infinite';
-import { useSearchParams } from 'next/navigation';
+import { Movie } from '@/types/movie';
+import getMovies from '../actions/movies/getMovies';
+import { PaginationResponse } from '@/types/api';
+import { ActionResponse } from '@/types/action';
 
-const PAGE_SIZE = 8;
-
-const getKey = (category: string | null) => (pageIndex: number, previousPageData: Movie[]) => {
-  if (previousPageData && !previousPageData.length) return null;
-
-  let url = `/api/movies?limit=${PAGE_SIZE}&offset=${pageIndex + 1}`;
-
-  if (category && category !== 'all') {
-    url += `&category=${category}`;
-  }
-  return url;
-};
-
-const fetcher = async (url: string) => {
-  const res = await fetch(`${ENV.API_BASE_URL}${url}`, {
-    cache: 'force-cache',
-  });
-
-  const data = await res.json();
-  return data;
-};
+const PAGE_SIZE = 5;
 
 export default function MoviesPage() {
   const searchParams = useSearchParams();
   const category = searchParams.get('category');
 
-  const [movies, setMovies] = useState<Movie[][]>([]);
-  const { data, size, setSize, isLoading } = useSWRInfinite<Movie[]>(getKey(category), fetcher);
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+  } = useInfiniteQuery<ActionResponse<PaginationResponse<Movie>>>({
+    initialPageParam: 1,
+    queryKey: ['movies', category],
+    queryFn: ({ pageParam }) => getMovies({
+      keyword: undefined,
+      category: category ?? undefined,
+      offset: pageParam as number ?? 1,
+      limit: PAGE_SIZE,
+    }),
+    getNextPageParam: (lastPage, allPages) => {
+      const nextPage = allPages.length + 1;
+      const totalPages = Math.ceil(lastPage.data.totalCount / PAGE_SIZE);
+      return nextPage <= totalPages ? nextPage : undefined;
+    }
+  });
 
-  useEffect(() => {
-    if (!data) return;
-
-    setMovies(data);
-  }, [isLoading, data, size]);
-
-  const isLast = movies && movies.length !== 0 && movies[movies.length - 1].length < PAGE_SIZE;
+  const movies = data?.pages.flatMap(page => page.data.data) ?? [];
 
   return (
     <main className={clsx('mt-[var(--header-height)] p-3')}>
@@ -54,9 +48,13 @@ export default function MoviesPage() {
       {isLoading && <div>Loading...</div>}
       <ul className="flex flex-col gap-[15px]">
         {movies.length > 0
-          ? data?.flat().map((movie, index) => <ScriptListItem key={index} {...movie} />)
+          ? movies.map((movie, index) => <ScriptListItem key={index} {...movie} />)
           : !isLoading && <div>No movies found</div>}
-        {!isLast && <Button onClick={() => setSize(size + 1)}>Load More</Button>}
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+            {isFetchingNextPage ? 'Loading more...' : 'Load More'}
+          </Button>
+        )}
       </ul>
     </main>
   );
--- apps/client/src/app/studies/page.tsx
+++ apps/client/src/app/studies/page.tsx
@@ -1,71 +1,10 @@
-'use client';
+import { Suspense } from "react";
+import StudiesPage from "./Studies";
 
-import React, { Suspense, useEffect, useState } from 'react';
-
-import Button from '@/components/Button';
-import StatusQueryTags from './_components/StatusQueryTags';
-import StudyItem from './_components/StudyItem';
-import clsx from 'clsx';
-import useSWRInfinite from 'swr/infinite';
-import { useSearchParams } from 'next/navigation';
-import { ListStudyDto } from '@renglish/services';
-
-const PAGE_SIZE = 4;
-
-const getKey = (status: string | null) => (pageIndex: number, previousPageData: ListStudyDto[]) => {
-  if (previousPageData && !previousPageData.length) return null;
-
-  let url = `/api/studies?limit=${PAGE_SIZE}&offset=${pageIndex + 1}`;
-  if (status) {
-    url += `&status=${status}`;
-  }
-
-  return url;
-};
-
-const fetcher = async (url: string) => {
-  const res = await fetch(`${url}`, {
-    cache: 'force-cache',
-  });
-
-  const data = await res.json();
-  return data;
-};
-
-export default function StudiesPage() {
+export default function Studies() {
   return (
     <Suspense>
-      <StudiesContent />
+      <StudiesPage />
     </Suspense>
-  );
-}
-
-function StudiesContent({}) {
-  const searchParams = useSearchParams();
-  const status = searchParams.get('status');
-
-  const [studies, setStudies] = useState<ListStudyDto[][]>([]);
-  const { data, size, setSize, isLoading } = useSWRInfinite<ListStudyDto[]>(
-    getKey(status),
-    fetcher,
-  );
-  useEffect(() => {
-    if (!data) return;
-
-    setStudies(data);
-  }, [isLoading, data, size]);
-
-  const isLast = studies && studies.length !== 0 && studies[studies.length - 1].length < PAGE_SIZE;
-
-  return (
-    <main className={clsx('mt-[var(--header-height)] p-3', 'flex flex-col gap-3')}>
-      <StatusQueryTags />
-      <div className="flex flex-col gap-3">
-        {studies.length > 0
-          ? data?.flat().map((study) => <StudyItem key={study.id} {...study} />)
-          : !isLoading && <div>No movies found</div>}
-        {!isLast && <Button onClick={() => setSize(size + 1)}>Load More</Button>}
-      </div>
-    </main>
   );
 }
