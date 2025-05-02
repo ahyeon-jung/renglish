561c27c6a0b02c4f85e7518ab99d9f503f2543ab
a-honey <jungahyeon0512@khu.ac.kr>
2025-05-03 01:24:03 +0900

    feat: 소켓 turn 서버 추가

--- apps/client/src/app/chat/page.tsx
+++ apps/client/src/app/chat/page.tsx
@@ -53,6 +53,11 @@ const WebRTCClient = () => {
         iceServers: [
           { urls: 'stun:stun.l.google.com:19302' },
           { urls: 'stun:stun1.l.google.com:19302' },
+          {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          }
         ],
       });
 
