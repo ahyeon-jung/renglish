17378c5e10062b5993596bbd4f2adc15b3ac5a5f
a-honey <jungahyeon0512@khu.ac.kr>
2025-05-03 22:44:08 +0900

    feat: 기타 turn 서버 추가

--- apps/client/src/app/chat/page.tsx
+++ apps/client/src/app/chat/page.tsx
@@ -56,12 +56,28 @@ const WebRTCClient = () => {
           {
             urls: 'turn:openrelay.metered.ca:80',
             username: 'openrelayproject',
-            'credential': 'openrelayproject'
+            credential: 'openrelayproject'
           },
           {
             urls: 'turn:openrelay.metered.ca:443',
             username: 'openrelayproject',
-            'credential': 'openrelayproject'
+            credential: 'openrelayproject'
+          },
+
+          {
+            urls: 'turn:numb.viagenie.ca',
+            username: 'webrtc@live.com',
+            credential: 'muazkh'
+          },
+          {
+            urls: 'turn:192.158.29.39:3478?transport=udp',
+            username: '28224511:1379330808',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA='
+          },
+          {
+            urls: 'turn:192.158.29.39:3478?transport=tcp',
+            username: '28224511:1379330808',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA='
           }
         ],
       });
